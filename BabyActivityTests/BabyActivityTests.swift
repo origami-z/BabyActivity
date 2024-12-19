a448eac6a9a72419bf55cfe973f6a1f0e40c82c7
origami-z <5257855+origami-z@users.noreply.github.com>
2024-12-19 09:09:19 +0000

    Initial Commit

---
+++ BabyActivityTests/BabyActivityTests.swift
@@ -0,0 +1,16 @@
+//
+//  BabyActivityTests.swift
+//  BabyActivityTests
+//
+//  Created by Zhihao Cui on 19/12/2024.
+//
+
+import Testing
+
+struct BabyActivityTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
